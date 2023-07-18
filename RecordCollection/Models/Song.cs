6b283c3e54c546f871f4203854f10b6e123278c1
Richard T <richard@turing.edu>
2023-07-18 17:47:55 -0400

    Update Song model and view

--- RecordCollection/Models/Song.cs
+++ RecordCollection/Models/Song.cs
@@ -6,5 +6,16 @@
         public string Title { get; set; }
         public int LengthInSeconds { get; set; }
         public Album Album { get; set; }
+
+        public string LengthInMinutesAndSeconds()
+        {
+            int minutes = LengthInSeconds / 60;
+            int seconds = LengthInSeconds % 60;
+
+            string minuteStr = (minutes < 10 ? "0" + minutes : minutes.ToString());
+            string secondStr = (seconds < 10 ? "0" + seconds : seconds.ToString());
+
+            return $"{minuteStr}:{secondStr}";
+        }
     }
 }
