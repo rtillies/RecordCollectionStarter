f3487056a4e16a90b3f0a795f1077000ba7ca057
Richard T <richard@turing.edu>
2023-07-18 16:09:37 -0400

    Add song Migrations

--- RecordCollection/Migrations/20230718200824_AddSongModel.Designer.cs
+++ RecordCollection/Migrations/20230718200824_AddSongModel.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RecordCollection.DataAccess;
@@ -11,9 +12,11 @@ using RecordCollection.DataAccess;
 namespace RecordCollection.Migrations
 {
     [DbContext(typeof(RecordCollectionContext))]
-    partial class RecordCollectionContextModelSnapshot : ModelSnapshot
+    [Migration("20230718200824_AddSongModel")]
+    partial class AddSongModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RecordCollection/Migrations/20230718200824_AddSongModel.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RecordCollection.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSongModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "songs",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    length_in_seconds = table.Column<int>(type: "integer", nullable: false),
+                    album_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_songs", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_songs_albums_album_id",
+                        column: x => x.album_id,
+                        principalTable: "albums",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_songs_album_id",
+                table: "songs",
+                column: "album_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "songs");
+        }
+    }
+}
