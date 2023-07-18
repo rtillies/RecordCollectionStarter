14f595cce9c7366a0d44410cdd0553ab51638119
Richard T <richard@turing.edu>
2023-07-18 16:42:41 -0400

    Songs Controller

---
+++ RecordCollection/Controllers/SongsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecordCollection.DataAccess;
+
+namespace RecordCollection.Controllers
+{
+    public class SongsController : Controller
+    {
+        private readonly RecordCollectionContext _context;
+
+        public SongsController(RecordCollectionContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /albums/:albumId/songs
+        [Route("Albums/{albumId:int}/Songs")]
+        public IActionResult Index(int albumId)
+        {
+            var album = _context.Albums
+                .Where(a => a.Id == albumId)
+                .Include(a => a.Songs)
+                .First();
+
+            return View(album);
+        }
+    }
+}
