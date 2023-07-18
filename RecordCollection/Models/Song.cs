c6b7d29ae70d9a4c2781dd974f97e26554e42ac3
Richard T <richard@turing.edu>
2023-07-18 16:07:37 -0400

    New Song model

---
+++ RecordCollection/Models/Song.cs
@@ -0,0 +1,10 @@
+﻿namespace RecordCollection.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int LengthInSeconds { get; set; }
+        public Album Album { get; set; }
+    }
+}
