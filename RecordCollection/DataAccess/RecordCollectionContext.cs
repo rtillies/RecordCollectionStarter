6b8c50dd983bb6fe66b4fd19d86f6b3345dafc6e
Richard T <richard@turing.edu>
2023-07-18 16:07:58 -0400

    Update context

--- RecordCollection/DataAccess/RecordCollectionContext.cs
+++ RecordCollection/DataAccess/RecordCollectionContext.cs
@@ -6,6 +6,7 @@ namespace RecordCollection.DataAccess
     public class RecordCollectionContext : DbContext
     {
         public DbSet<Album> Albums { get; set; }
+        public DbSet<Song> Songs { get; set; }
         public RecordCollectionContext(DbContextOptions<RecordCollectionContext> options) : base(options) { }
     }
 }
